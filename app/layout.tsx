137da52f83e6566b9f032126ba4b1a54295d5023
‘nojoon’ <nojoon@gmail.com>
2026-09-11 23:52:46 +0900

    block robots.txt

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,16 @@ import './globals.css';
 export const metadata: Metadata = {
   title: '가지 랩스 회의록',
   description: '녹음 파일을 업로드하면 회의록을 만들어 드립니다.',
+  robots: {
+    index: false,
+    follow: false,
+    nocache: true,
+    googleBot: {
+      index: false,
+      follow: false,
+      noimageindex: true,
+    },
+  },
 };
 
 export const viewport: Viewport = {
